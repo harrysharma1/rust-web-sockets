8857a0458fb012171d3fec96e228f465a5b80f9f
Harry Sharma <harrysharma1066@gmail.com>
2024-02-29 14:40:51 +0000

    feat create client struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::usize;
+use tokio::sync::mpsc;
+use warp::filters::ws::Message;
+mod websocket;
+mod handler;
+
+pub struct Client{
+    pub uid: usize,
+    pub topics: Vec<String>,
+    pub sender: Option<tokio::sync::mpsc::UnboundedSender<std::result::Result<Message, warp::Error>>>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
